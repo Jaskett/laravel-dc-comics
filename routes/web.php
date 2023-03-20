c624e6b107446a951ce8090cbadc953c30ba5d2a
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-20 12:06:04 +0100

    Creazione index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Controllers
+use App\Http\Controllers\ComicController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicController::class, 'index'])->name('home');
+
+Route::resource('comics', ComicController::class);

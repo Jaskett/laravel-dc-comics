c0473bc15e7f978ff665e40386ff88406e1dfcae
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-20 11:57:29 +0100

    Creato Seeder

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Models
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_comics = config('comics');
+
+        foreach ($array_comics as $comic_item) {
+            $new_comic = new Comic();
+            $new_comic->title = $comic_item['title'];
+            $new_comic->slug = Comic::generateSlug($new_comic->title);
+            $new_comic->thumb = $comic_item['thumb'];
+            $new_comic->description = $comic_item['description'];
+            $new_comic->series = $comic_item['series'];
+            $new_comic->price = $comic_item['price'];
+            $new_comic->sale_date = $comic_item['sale_date'];
+            $new_comic->type = $comic_item['type'];
+            $new_comic->save();
+        }
+    }
+}
